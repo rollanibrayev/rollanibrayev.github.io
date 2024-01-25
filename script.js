cda8ab735e9e1033401c6a938d15caa1ecc6b27f
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-01-25 18:54:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-function extractChannelName(fullText)
-{
+function extractChannelName(fullText) {
   let lastSlashIndex = fullText.lastIndexOf('/');
   return (lastSlashIndex !== -1) ? fullText.substring(lastSlashIndex + 1) : fullText;
 }
   
-function giveIframeChannel(channel)
-{
+function giveIframeChannel(channel) {
   const iframe = document.getElementById("iframe");
   iframe.src = `https://player.twitch.tv/?channel=${channel}&enableExtensions=true&muted=false&parent=rollanibrayev.github.io&player=popout&quality=720p60&volume=1`;
   iframe.style = `display: block`;
 }
 
-function generateIframes()
-{
+function generateIframes() {
   let channel = extractChannelName(document.getElementById("channel").value.trim());
-  if (channel)
-  {
+  if (channel) {
     giveIframeChannel(channel);
     document.getElementById("channelForm").style.display = "none";
-  } else
-  {
+  } else {
     alert("Please enter a channel name.");
   }
 }
