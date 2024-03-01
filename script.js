7844555548f0bfdc81d1ed597f79164ef6eaacb1
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-01 12:48:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,7 @@ document.addEventListener('keydown', (event) => {
     finalizeIframe();
   }
   if (event.key === 'v' && event.ctrlKey) {
-    [];
-    finalizeIframe();
+    setTimeout(finalizeIframe, 1);
   }
 });
 document.querySelector('input[type="button"]').addEventListener('click', finalizeIframe);
