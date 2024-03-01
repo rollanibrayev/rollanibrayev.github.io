0c631dada17c164d728feb61acb007bf5b2105bf
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-01 20:41:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-textInput = document.querySelector(`input[type='text']`);
+textInput = document.querySelector(`input[type='text']`)
 finalizeIframe = () => {
   trimmedText = t.value.trim()
   lastIndexOf = trimmedText.lastIndexOf('/')
   channel = (lastIndexOf !== -1) ? trimmedText.substring(lastIndexOf + 1) : trimmedText
   if (channel) {
-    r = document.querySelector('iframe')
-    r.src = `https://player.twitch.tv/?channel=${channel}&muted=false&parent=rollanibrayev.github.io&player=popout&quality=chunked&volume=1`
-    r.style = 'display: block'
+    iframe = document.querySelector('iframe')
+    iframe.src = `https://player.twitch.tv/?channel=${channel}&muted=false&parent=rollanibrayev.github.io&player=popout&quality=chunked&volume=1`
+    iframe.style = 'display: block'
     document.querySelector('form').style.display = 'none'
     document.body.style.justifyContent = 'left'
     document.body.style.alignItems = 'start'
