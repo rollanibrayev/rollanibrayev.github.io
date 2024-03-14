baaa84c0f205e43543cf2cd6b4be3acd1a327930
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-14 12:49:08 +0000

    f

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document.querySelector('input').addEventListener('input', event => {
     if (generatedIframesCount < 1) event.target.removeAttribute('placeholder')
     if (generatedIframesCount == 2) document.body.requestFullscreen()
     const iframe = document.querySelectorAll('iframe')[++generatedIframesCount - 1]
-    iframe.src = `https://player.twitch.tv/?enableExtensions=true&muted=true&parent=rollanibrayev.github.io&player=popout&quality=chunked&volume=1&channel=${event.target.value.split('/').pop()}`
+    iframe.src =
+        'https://player.twitch.tv/?enableExtensions=true&muted=true&' +
+        'parent=rollanibrayev.github.io&player=popout&quality=chunked&volume=1&channel=' +
+        event.target.value.split('/').pop()
     event.target.value = ''
     if (generatedIframesCount > 3) event.target.remove()
 })
