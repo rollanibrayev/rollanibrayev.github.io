e4b5ecef506262e97fec98ac204ab939a1abcc3f
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-14 13:05:39 +0000

    df

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let generatedIframesCount = 0
 document.querySelector('input').addEventListener('input', event => {
     if (generatedIframesCount < 1) event.target.removeAttribute('placeholder')
-    if (generatedIframesCount == 2) document.body.requestFullscreen()
+    if (generatedIframesCount == 2) {
+        document.body.requestFullscreen()
+        document.querySelectorAll('iframe')[0].style = 'left:  0'
+    }
     const iframe = document.querySelectorAll('iframe')[++generatedIframesCount - 1]
     iframe.src =
         'https://player.twitch.tv/?enableExtensions=true&muted=true&' +
@@ -10,3 +13,7 @@ document.querySelector('input').addEventListener('input', event => {
     event.target.value = ''
     if (generatedIframesCount > 3) event.target.remove()
 })
+// iframe:nth-of-type(1) { left:  0;   top:  0   }
+// iframe:nth-of-type(2) { left: 50vw; top:  0   }
+// iframe:nth-of-type(3) { left:  0;   top: 50vh }
+// iframe:nth-of-type(4) { left: 50vw; top: 50vh }
