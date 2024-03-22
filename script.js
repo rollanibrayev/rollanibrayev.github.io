f7380c418981ee37ab0b0ace13ffd84219213a61
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-22 21:59:14 +0000

    '

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 document.body.style = 'overflow: hidden'
 let appearedIframesCount = 0
-const
-iframes = document.querySelectorAll('iframe'),
-fullscreen = event => !document.fullscreenElement ? document.body.requestFullscreen() :1,
-toggle = event => {
+const iframes = document.querySelectorAll('iframe')
+const fullscreen = event => {
+  if (!document.fullscreenElement) document.body.requestFullscreen()
+}
+const toggle = event => {
   event.preventDefault()
   document.fullscreenElement
     ? document.exitFullscreen()
     : document.body.requestFullscreen()
-},
-listener = event => event.key === 'F' || event.key === 'f' ? toggle(event) :1,
-u = (m, ...a) => {
+}
+const listener = event => {
+  if (event.key === 'F' || event.key === 'f') toggle(event)
+}
+const inputListener = event => {
+  if (appearedIframesCount < 1) {
+    document.body.removeAttribute('style')
+    event.target.removeAttribute('placeholder')
+    fullscreen()
+    iframes[0].style = `
+      width: 100vw;
+      height: 56.25vw
+    `
+  }
+  if (appearedIframesCount == 1) {
+    window.scrollTo(0, 0)
+    document.body.style = 'overflow: hidden'
+    iframes[0].style = `
+      top: 50.000vh;
+      transform: translateY(-50%)
+    `
+    if (document.fullscreenElement) document.exitFullscreen()
+  }
+  if (appearedIframesCount == 2) {
+    [document.body, iframes[0]].forEach(e => e.removeAttribute('style'))
+    iframes[1].style = 'top: 0; transform: translateY(0)'
+  }
+  if (appearedIframesCount == 3) iframes[2].style = 'left: 0'
+  iframes[++appearedIframesCount - 1].src =
+    'https://player.twitch.tv/?muted=true&volume=0.25&quality=chunked&' +
+    'parent=rollanibrayev.github.io&channel=' +
+    event.target.value.split('/').pop()
+  if (appearedIframesCount == 3) fullscreen()
+  event.target.value = ''
+  if (appearedIframesCount > 3) {
+    fullscreen()
+    event.target.setAttribute('readonly', true)
+    event.target.removeEventListener('input', inputListener)
+  }
+}
+document.querySelector('input').addEventListener('input', inputListener)
+document.addEventListener('keydown', listener)
+document.addEventListener('dblclick', toggle)
+const u = (m, ...a) => {
   let n =
     m[0] == 'r'
     ? 2
@@ -21,44 +63,4 @@ u = (m, ...a) => {
       ? a[i + 1]
       : a[i + 1], a[i + 2]
     )
-},
-inputListener = event => {
-  if (appearedIframesCount < 1)
-    u( 'removeAttribute',
-      document.body, 'style',
-      event.target, 'placeholder'
-    ),
-    fullscreen(),
-    iframes[0].style = `
-      width: 100vw;
-      height: 56.25vw
-    `
-  if (appearedIframesCount == 1) {
-    window.scrollTo(0, 0)
-    document.body.style = 'overflow: hidden'
-    iframes[0].style = `
-      top: 50.000vh;
-      transform: translateY(-50%)
-    `
-    document.fullscreenElement ? document.exitFullscreen() :1
-  }
-  if (appearedIframesCount == 2)
-    [document.body, iframes[0]].forEach(e => u('removeAttribute', e, 'style')), 
-    iframes[1].style = 'top: 0; transform: translateY(0)'
-  if (appearedIframesCount == 3) iframes[2].style = 'left: 0'
-  iframes[++appearedIframesCount - 1].src =
-    'https://player.twitch.tv/?muted=true&volume=0.25&quality=chunked&' +
-    'parent=rollanibrayev.github.io&channel=' +
-    event.target.value.split('/').pop()
-  appearedIframesCount == 3 ? fullscreen() :1
-  event.target.value = ''
-  if (appearedIframesCount > 3)
-    fullscreen(),
-    event.target.setAttribute('readonly', true),
-    event.target.removeEventListener('input', inputListener)
 }
-u( 'addEventListener',
-  document.querySelector('input'), 'input', inputListener,
-  document, 'keydown', listener,
-  document, 'dblclick', toggle
-)
