de6d074e2aeeb95bf55939132fcd771d84a27414
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-24 11:56:52 +0000

    f

--- 768/script.js
+++ 768/script.js
@@ -12,7 +12,7 @@ fullsize = iframeNumber => {
   const iframe = iframes[iframeNumber]
   iframe.style += style
 },
-channels = [],
+appearedIframesCounter = 0,
 fullscreen = event => !document.fullscreenElement ? document.body.requestFullscreen() :1,
 toggle = event => {
   event.preventDefault()
@@ -32,36 +32,42 @@ u = (m, ...a) => {
       : a[i + 1], a[i + 2]
     )
 },
+isMuted = src => src[26] == 'm' ? 1 : 0,
 smallInput = (event, iframeNumber) => {
-  if (event.target.value == 'm' || event.target.value == 'M') {
-    Array.from(String(event.target.value)).forEach(iframeNumber => {
-      const
-      iframe = iframes[iframeNumber - 1],
-      src = iframe.src;
-      iframe.src =
-        src[26] == 'm'
+  const value = event.target.value
+  if (value == 'm' || value == 'M') {
+    iframes[iframeNumber].src =
+      isMuted(iframes[iframeNumber].src)
         ? start + src.slice(37)
         : start + 'muted=true&' + src.slice(25)
-    })
   }
-  if (event.target.value.length > 1) {
-    iframes[iframeNumber - 1].src = start + muted + 'quality=720p60&' +
-    'parent=rollanibrayev.github.io&channel=' + event.target.value.split('/').pop()
+  if (value == 'c' || value == 'C') {
+    let channel
+    let url = `https://www.twitch.tv/popout/${channel}/chat`
+    channel =
+      isMuted(iframes[iframeNumber].src)
+     // ? 
+      //: 
+    window.open(url, '_blank')
   }
-  if (event.target.value == 'r' || event.target.value == 'R')
+  if (value.length > 1) {
+    iframes[iframeNumber].src = start + muted + 'quality=720p60&' +
+      'parent=rollanibrayev.github.io&channel=' + value.split('/').pop()
+  }
+  if (value == 'r' || value == 'R')
     for (let i = 0; i < iframes.length; i++)
       iframes[i].src = iframes[i].src
   event.target.value = ''
 }
 inputs[5 - 1].addEventListener('input', event => {
-  if (channels.length < 1)
+  if (appearedIframesCounter < 1)
     u( 'removeAttribute',
       document.body, 'style',
       event.target, 'placeholder'
     ),
     fullscreen(),
     iframes[0].style = style
-  if (channels.length == 1) {
+  if (appearedIframesCounter == 1) {
     window.scrollTo(0, 0)
     document.body.style = 'overflow: hidden'
     iframes[0].style = `
@@ -70,24 +76,23 @@ inputs[5 - 1].addEventListener('input', event => {
     `
     document.fullscreenElement ? document.exitFullscreen() :1
   }
-  if (channels.length == 2)
+  if (appearedIframesCounter == 2)
     [document.body, iframes[0]].forEach(e => u('removeAttribute', e, 'style')), 
     iframes[1].style = 'top: 0; transform: translateY(0)'
-  if (channels.length == 3) iframes[2].style = 'left: 0'
-  channels.push(event.target.value.split('/').pop())
-  iframes[channels.length - 1].src =
+  if (appearedIframesCounter == 3) iframes[2].style = 'left: 0'
+  iframes[++appearedIframesCounter - 1].src =
     start + muted + 'quality=720p60&' +
     'parent=rollanibrayev.github.io&channel=' +
-    channels[channels.length - 1]
-  channels.length == 3 ? fullscreen() :1
+    event.target.value.split('/').pop()
+  appearedIframesCounter == 3 ? fullscreen() :1
   event.target.value = ''
-  if (channels.length > 3)
+  if (appearedIframesCounter > 3)
     fullscreen(),
     event.target.remove()
 })
 document.addEventListener('keydown', event => event.key === 'F' || event.key === 'f' ? toggle(event) :1)
 document.addEventListener('dblclick', toggle)
-inputs[1 - 1].addEventListener('input', event => smallInput(event, 1))
-inputs[2 - 1].addEventListener('input', event => smallInput(event, 2))
-inputs[3 - 1].addEventListener('input', event => smallInput(event, 3))
-inputs[4 - 1].addEventListener('input', event => smallInput(event, 4))
+inputs[1 - 1].addEventListener('input', event => smallInput(event, 0))
+inputs[2 - 1].addEventListener('input', event => smallInput(event, 1))
+inputs[3 - 1].addEventListener('input', event => smallInput(event, 2))
+inputs[4 - 1].addEventListener('input', event => smallInput(event, 3))
