3c2a1a313b68464680497fb09ab83473e055f822
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-27 14:47:38 +0000

    Update script.js

--- 768/script.js
+++ 768/script.js
@@ -87,7 +87,7 @@ const smallInput = (event, iframeNumber) => {
 }
 let clickedIframeNumber
 
-const smallInputClick = iframeNumber => {
+const smallInputMouseDown = iframeNumber => {
   clickedIframeNumber = iframeNumber
 }
 
@@ -99,15 +99,15 @@ const smallInputMouseUp = iframeNumber => {
   iframes[iframeNumber].src = clickedIframeSrc
 }
 
-inputs[0].addEventListener('click', event => { smallInputClick(0) } )
-inputs[1].addEventListener('click', event => { smallInputClick(1) } )
-inputs[2].addEventListener('click', event => { smallInputClick(2) } )
-inputs[3].addEventListener('click', event => { smallInputClick(3) } )
+inputs[0].addEventListener('mousedown', event => smallInputMouseDown(0))
+inputs[1].addEventListener('mousedown', event => smallInputMouseDown(1))
+inputs[2].addEventListener('mousedown', event => smallInputMouseDown(2))
+inputs[3].addEventListener('mousedown', event => smallInputMouseDown(3))
 
-inputs[0].addEventListener('mouseup', event => { smallInputMouseUp(0) } )
-inputs[1].addEventListener('mouseup', event => { smallInputMouseUp(1) } )
-inputs[2].addEventListener('mouseup', event => { smallInputMouseUp(2) } )
-inputs[3].addEventListener('mouseup', event => { smallInputMouseUp(3) } )
+inputs[0].addEventListener('mouseup', event => smallInputMouseUp(0))
+inputs[1].addEventListener('mouseup', event => smallInputMouseUp(1))
+inputs[2].addEventListener('mouseup', event => smallInputMouseUp(2))
+inputs[3].addEventListener('mouseup', event => smallInputMouseUp(3))
 
 inputs[4].addEventListener('input', event => {
   if (appearedIframesCounter < 1) {
