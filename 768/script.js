5d6d4c9e2fbbfd9fe63d05b525057b13ee227765
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-29 10:02:39 +0000

    Update script.js

--- 768/script.js
+++ 768/script.js
@@ -1,4 +1,3 @@
-document.body.style = 'overflow: hidden'
 let appearedIframesCounter = 0
 const iframes = document.querySelectorAll('iframe')
 const inputs = document.querySelectorAll('input')
@@ -23,10 +22,6 @@ const isLowerQuality = url => {
     : 0
 }
 const extractChannel = url => url.split('/').pop()
-const style = `
-  width: calc(64000vw/683);
-  height: calc(36000vw/683)
-`
 const fullsize = iframeNumber => {
   const iframe = iframes[iframeNumber]
   iframe.style += style
@@ -41,36 +36,13 @@ const toggle = event => {
 
 
 inputs[4].addEventListener('input', event => {
-  if (appearedIframesCounter < 1) {
-    document.body.removeAttribute('style')
+  fullscreen()
+  if (appearedIframesCounter < 1)
     event.target.removeAttribute('placeholder')
-    fullscreen()
-    iframes[0].style = style
-  }
-  switch (appearedIframesCounter) {
-    case 1:
-      window.scrollTo(0, 0)
-      document.body.style = 'overflow: hidden'
-      iframes[0].style = `
-        top: calc(18000vw/683);
-        transform: translateY(-50%)
-      `
-      document.fullscreenElement ? document.exitFullscreen() :1
-    break
-    case 2:
-      [document.body, iframes[0]].forEach(e => e.removeAttribute('style'))
-      iframes[1].style = 'top: 0; transform: translateY(0)'
-    break
-    case 3:
-      iframes[2].style = 'left: 0'
-      fullscreen()
-  }
   iframes[++appearedIframesCounter - 1].src =
     urlConfig.start + urlConfig.muted + urlConfig.quality.high + urlConfig.middle2 + extractChannel(event.target.value)
-  if (appearedIframesCounter == 3) fullscreen()
   event.target.value = ''
   if (appearedIframesCounter > 3)
-    fullscreen(),
     event.target.remove(),
     appearedIframesCounter = undefined
 })
@@ -80,71 +52,68 @@ const topCoordinate = 'calc(18000vw/683)'
 inputs[1].style = `left: ${leftCoordinate}`
 inputs[2].style = `top: ${topCoordinate}`
 inputs[3].style = `left: ${leftCoordinate}; top: ${topCoordinate}`
-
-const smallInput = (event, iframeNumber) => {
-  const value = event.target.value
-  const iframe = iframes[iframeNumber]
-  const src = iframe.src
-  const i9 = urlConfig.middle2 + src.slice(91)
-  const i8 = urlConfig.middle2 + src.slice(80)
-  const sm = urlConfig.start + urlConfig.muted
-  switch (value.toLowerCase()) {
-    case 'm':
-      iframe.src =
-        isMuted(src)
-        ? urlConfig.start + src.slice(37)
-        : sm + src.slice(26)
-      break
-    case 'q':
-      iframe.src =
-        isLowerQuality(src)
-        ? isMuted(src)
-          ? sm + urlConfig.quality.high + i9
-          : urlConfig.start + urlConfig.quality.high + i8
-        : isMuted(src)
-          ? sm + urlConfig.quality.low + i9
-          : urlConfig.start + urlConfig.quality.low + i8
-      break
-    case 'c':
-      window.open(
-        `https://www.twitch.tv/popout/${
-          isMuted(src) ? src.slice(91) : src.slice(80)
-        }/chat`,
-        '_blank'
-      )
-      break
-    case 'r':
-      iframes.forEach(iframe => iframe.src = iframe.src)
-      break
-    case 'k':
-      iframe.src = iframe.src
-      break
-    case 'd':
-      iframe.removeAttribute('src')
-      break
-    default:
-      if (value[1])
-        iframe.src = sm + urlConfig.quality.high + urlConfig.middle2 + extractChannel(value)
-  }
-  event.target.value = ''
-}
+iframes[1].style = `left: ${leftCoordinate}`
+iframes[2].style = `top: ${topCoordinate}`
+iframes[3].style = `left: ${leftCoordinate}; top: ${topCoordinate}`
 
 let mouseDownedIframeNumber
-const smallInputMouseUp = iframeNumber => {
-  if (mouseDownedIframeNumber == iframeNumber) return
-  const mouseUppedIframeSrc = iframes[iframeNumber].src
-  iframes[iframeNumber].src = iframes[mouseDownedIframeNumber].src
-  iframes[mouseDownedIframeNumber].src = mouseUppedIframeSrc
-  mouseDownedIframeNumber = undefined
-}
-
 inputs.forEach(
   (input, index) => {
     input.addEventListener('mousedown', () => mouseDownedIframeNumber = index )
-    input.addEventListener('mouseup', () => smallInputMouseUp(index))
-    input.addEventListener('input', event => smallInput(event, index))
+    input.addEventListener('mouseup', () => {
+      if (mouseDownedIframeNumber == index) return
+      const mouseUppedIframeStyle = iframes[index].style
+      iframes[index].style = iframes[mouseDownedIframeNumber].style
+      iframes[mouseDownedIframeNumber].style = mouseUppedIframeStyle
+    })
+    input.addEventListener('input', event => {
+      const value = event.target.value
+      const iframe = iframes[index]
+      const src = iframe.src
+      const i9 = urlConfig.middle2 + src.slice(91)
+      const i8 = urlConfig.middle2 + src.slice(80)
+      const sm = urlConfig.start + urlConfig.muted
+      switch (value.toLowerCase()) {
+        case 'm':
+          iframe.src =
+            isMuted(src)
+            ? urlConfig.start + src.slice(37)
+            : sm + src.slice(26)
+          break
+        case 'q':
+          iframe.src =
+            isLowerQuality(src)
+            ? isMuted(src)
+              ? sm + urlConfig.quality.high + i9
+              : urlConfig.start + urlConfig.quality.high + i8
+            : isMuted(src)
+              ? sm + urlConfig.quality.low + i9
+              : urlConfig.start + urlConfig.quality.low + i8
+          break
+        case 'c':
+          window.open(
+            `https://www.twitch.tv/popout/${
+              isMuted(src) ? src.slice(91) : src.slice(80)
+            }/chat`,
+            '_blank'
+          )
+          break
+        case 'r':
+          iframes.forEach(iframe => iframe.src = iframe.src)
+          break
+        case 'k':
+          iframe.src = iframe.src
+          break
+        case 'd':
+          iframe.removeAttribute('src')
+          break
+        default:
+          if (value[1])
+            iframe.src = sm + urlConfig.quality.high + urlConfig.middle2 + extractChannel(value)
+      }
+      event.target.value = ''
+    })
   }
 )
-
 document.addEventListener('keydown', event => event.key.toLowerCase() == 'f' ? toggle(event) :1)
 document.addEventListener('dblclick', toggle)
