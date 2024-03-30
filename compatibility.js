99cf50f49c773dbee5a153998ae57e58efe58135
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-03-30 16:53:49 +0000

    Delete compatibility.js

--- compatibility.js
+++
@@ -1,7 +0,0 @@
-const is768 = window.screen.height == 768
-const leftCoordinate = `calc(${is768 ? '32000vw/683' : '50vw'})`
-const topCoordinate = `calc(${is768 ? '18000vw/683' : '28.125vw'})`
-const quality = {
-  low: `quality=${is768 ? '360p30' : '720p60'}&`,
-  high: `quality=${is768 ? '720p60' : '1080p60'}&`
-}
