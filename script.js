7a6866e2920d30169d37e085441fe80458bf62d7
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-04-08 09:08:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
 const is768 = window.screen.height == 768
 const leftCoordinate = `calc(${ is768 ? '32000vw/683' : '50vw' })`
 const topCoordinate = `calc(${ is768 ? '18000vw/683' : '28.125vw' })`
-let appearedIframesCounter = 0
 const iframes = document.querySelectorAll('iframe')
 iframes.forEach(iframe => {
   iframe.style.width = leftCoordinate
   iframe.style.height = topCoordinate
 })
-const inputs = document.querySelectorAll('input')
-
 iframes[1].style.left = leftCoordinate
 iframes[2].style.top = topCoordinate
 iframes[3].style.left = leftCoordinate
 iframes[3].style.top = topCoordinate
+const inputs = document.querySelectorAll('input')
 inputs[1].style.left = leftCoordinate
 inputs[2].style.top = topCoordinate
 inputs[3].style.left = leftCoordinate
 inputs[3].style.top = topCoordinate
-
-const urlConfig = {
-  start: 'https://player.twitch.tv/?',
-  muted: 'muted=true&',
-  quality: {
-    low: `quality=${ is768 ? '360p30' : '720p60' }&`,
-    high: `quality=${ is768 ? '720p60' : 'chunked' }&`
-  },
-  middle2: 'parent=rollanibrayev.github.io&channel='
-}
-const isMuted = src => src[26] == 'm' ? 1 : 0
-const isLowerQuality = src => {
-  const indicator = urlConfig.quality.low[8]
-  return isMuted(src)
-  ? src[45] == indicator
-    ? 1
-    : 0
-  : src[34] == indicator
-    ? 1
-    : 0
-}
-const extractChannel = url => url.slice(22)
-const toggle = event => {
-  event.preventDefault()
-  document.fullscreenElement
-  ? document.exitFullscreen()
-  : document.body.requestFullscreen()
-}
-const fullscreen = () => !document.fullscreenElement ? document.body.requestFullscreen() : null
-let mouseDownedInput
+let appearedIframesCounter = 0
+let mouseDownedInputIndex
 inputs[4].addEventListener('input', event => {
+  const fullscreen = () => !document.fullscreenElement ? document.body.requestFullscreen() : null
   if (appearedIframesCounter > 1) fullscreen()
   if (appearedIframesCounter < 1) event.target.removeAttribute('placeholder')
+  const urlConfig = {
+    start: 'https://player.twitch.tv/?',
+    muted: 'muted=true&',
+    quality: {
+      low: `quality=${ is768 ? '360p30' : '720p60' }&`,
+      high: `quality=${ is768 ? '720p60' : 'chunked' }&`
+    },
+    middle2: 'parent=rollanibrayev.github.io&channel='
+  }
+  const extractChannel = url => url.slice(22)
   iframes[++appearedIframesCounter - 1].src =
     urlConfig.start + urlConfig.muted + urlConfig.quality.high + urlConfig.middle2 + extractChannel(event.target.value)
   event.target.value = ''
@@ -58,19 +39,19 @@ inputs[4].addEventListener('input', event => {
     inputs.forEach( (input, index) => {
       input.style.width = leftCoordinate
       input.style.height = topCoordinate
-      input.addEventListener('mousedown', () => mouseDownedInput = index )
+      input.addEventListener('mousedown', () => mouseDownedInputIndex = index )
       input.addEventListener('mouseup', () => {
-        if (mouseDownedInput == index) return
+        if (mouseDownedInputIndex == index) return
         const mouseUppedQuarterLeftCoordinate = iframes[index].style.left
         const mouseUppedQuarterTopCoordinate = iframes[index].style.top
-        iframes[index].style.left = iframes[mouseDownedInput].style.left
-        iframes[index].style.top = iframes[mouseDownedInput].style.top
-        inputs[index].style.left = inputs[mouseDownedInput].style.left
-        inputs[index].style.top = inputs[mouseDownedInput].style.top
-        iframes[mouseDownedInput].style.left = mouseUppedQuarterLeftCoordinate
-        iframes[mouseDownedInput].style.top = mouseUppedQuarterTopCoordinate
-        inputs[mouseDownedInput].style.left = mouseUppedQuarterLeftCoordinate
-        inputs[mouseDownedInput].style.top = mouseUppedQuarterTopCoordinate
+        iframes[index].style.left = iframes[mouseDownedInputIndex].style.left
+        iframes[index].style.top = iframes[mouseDownedInputIndex].style.top
+        inputs[index].style.left = inputs[mouseDownedInputIndex].style.left
+        inputs[index].style.top = inputs[mouseDownedInputIndex].style.top
+        iframes[mouseDownedInputIndex].style.left = mouseUppedQuarterLeftCoordinate
+        iframes[mouseDownedInputIndex].style.top = mouseUppedQuarterTopCoordinate
+        inputs[mouseDownedInputIndex].style.left = mouseUppedQuarterLeftCoordinate
+        inputs[mouseDownedInputIndex].style.top = mouseUppedQuarterTopCoordinate
       })
       input.addEventListener('input', event => {
         const value = event.target.value
@@ -81,6 +62,17 @@ inputs[4].addEventListener('input', event => {
         const sm = urlConfig.start + urlConfig.muted
         const l = urlConfig.quality.low
         const h = urlConfig.quality.high
+        const isMuted = src => src[26] == 'm' ? 1 : 0
+        const isLowerQuality = src => {
+          const indicator = urlConfig.quality.low[8]
+          return isMuted(src)
+          ? src[45] == indicator
+            ? 1
+            : 0
+          : src[34] == indicator
+            ? 1
+            : 0
+        }
         switch (value.toUpperCase()) {
           case 'M':
             iframe.src =
@@ -123,5 +115,11 @@ inputs[4].addEventListener('input', event => {
       })
     } )
 })
+const toggle = event => {
+  event.preventDefault()
+  document.fullscreenElement
+  ? document.exitFullscreen()
+  : document.body.requestFullscreen()
+}
 document.addEventListener('keydown', event => event.key.toUpperCase() == 'F' ? toggle(event) :1)
 document.addEventListener('dblclick', toggle)
