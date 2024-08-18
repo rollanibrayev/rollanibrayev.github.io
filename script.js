e36c3bf49951c5b1234eacaca830e83e9101d10a
rollanibrayev <156797517+rollanibrayev@users.noreply.github.com>
2024-08-18 16:11:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const iframes = document.querySelectorAll('iframe')
-const is768 = window.screen.height == 768
+const iframes        = document.querySelectorAll('iframe')
+const is768          = window.screen.height == 768
 const leftCoordinate = `calc(${ is768 ? '32000vw/683' : '50vw' })`
-const topCoordinate = `calc(${ is768 ? '18000vw/683' : '28.125vw' })`
-const inputs = document.querySelectorAll('input')
+const topCoordinate  = `calc(${ is768 ? '18000vw/683' : '28.125vw' })`
+const inputs         = document.querySelectorAll('input')
 
 iframes.forEach(iframe => {
-  iframe.style.width = leftCoordinate
+  iframe.style.width  = leftCoordinate
   iframe.style.height = topCoordinate
 })
 iframes[1].style.left = leftCoordinate
-iframes[2].style.top = topCoordinate
+iframes[2].style.top  = topCoordinate
 iframes[3].style.left = leftCoordinate
-iframes[3].style.top = topCoordinate
-inputs[1].style.left = leftCoordinate
-inputs[2].style.top = topCoordinate
-inputs[3].style.left = leftCoordinate
-inputs[3].style.top = topCoordinate
+iframes[3].style.top  = topCoordinate
+inputs [1].style.left = leftCoordinate
+inputs [2].style.top  = topCoordinate
+inputs [3].style.left = leftCoordinate
+inputs [3].style.top  = topCoordinate
 
-const start = 'https://player.twitch.tv/?'
-const muted = 'muted=true&'
-const low = `quality=${ is768 ? '360p30' : '720p60' }&`
-const high = `quality=${ is768 ? '720p60' : 'chunked' }&`
-const middle = 'parent=rollanibrayev.github.io&channel='
+const start          = 'https://player.twitch.tv/?'
+const muted          = 'muted=true&'
+const low            = `quality=${ is768 ? '360p30' : '720p60' }&`
+const high           = `quality=${ is768 ? '720p60' : 'chunked' }&`
+const middle         = 'parent=rollanibrayev.github.io&channel='
 const extractChannel = url => url.slice(22)
 
 let appearedIframesCounter = 0
@@ -38,21 +38,21 @@ inputs[4].addEventListener('input', event => {
 let mouseDownedInputIndex
 inputs.forEach((input, index) => {
   if (index > 3) return
-  input.style.width = leftCoordinate
+  input.style.width  = leftCoordinate
   input.style.height = topCoordinate
   input.addEventListener('mousedown', () => mouseDownedInputIndex = index )
   input.addEventListener('mouseup', () => {
     if (mouseDownedInputIndex == index) return
     const mouseUppedInputLeftCoordinate = iframes[index].style.left
-    const mouseUppedInputTopCoordinate = iframes[index].style.top
+    const mouseUppedInputTopCoordinate  = iframes[index].style.top
     iframes[index].style.left = iframes[mouseDownedInputIndex].style.left
-    iframes[index].style.top = iframes[mouseDownedInputIndex].style.top
-    inputs[index].style.left = inputs[mouseDownedInputIndex].style.left
-    inputs[index].style.top = inputs[mouseDownedInputIndex].style.top
+    iframes[index].style.top  = iframes[mouseDownedInputIndex].style.top
+    inputs [index].style.left = inputs [mouseDownedInputIndex].style.left
+    inputs [index].style.top  = inputs [mouseDownedInputIndex].style.top
     iframes[mouseDownedInputIndex].style.left = mouseUppedInputLeftCoordinate
-    iframes[mouseDownedInputIndex].style.top = mouseUppedInputTopCoordinate
-    inputs[mouseDownedInputIndex].style.left = mouseUppedInputLeftCoordinate
-    inputs[mouseDownedInputIndex].style.top = mouseUppedInputTopCoordinate
+    iframes[mouseDownedInputIndex].style.top  = mouseUppedInputTopCoordinate
+    inputs [mouseDownedInputIndex].style.left = mouseUppedInputLeftCoordinate
+    inputs [mouseDownedInputIndex].style.top  = mouseUppedInputTopCoordinate
   })
   input.addEventListener('input', event => {
     const value = event.target.value
